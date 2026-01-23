c82cf84669f43f7df5ccb63f66c7c30bc18fc570
lenosza <varganorbert1605@gmail.com>
2026-01-23 18:23:15 +0100

    Update tables.sql
    Játékhoz szükséges adatbázisok megcsinálása

--- database/tables.sql
+++ database/tables.sql
@@ -11,3 +11,29 @@ CREATE TABLE users (
     2fa BOOLEAN DEFAULT 0,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE map {
+    mapid int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    creatorid int,
+    name varchar(50) NOT NULL,
+    picid int,
+    foreign key (creatorid) references users(userid) ON DELETE SET NULL,
+    foreign key (picid) references pictures(picid) ON DELETE SET NULL
+}
+
+CREATE TABLE points {
+    pointid int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    mapid int,
+    pointx int NOT NULL,
+    pointy int NOT NULL,
+    picid int,
+    foreign key (mapid) references map(mapid) ON DELETE SET NULL,
+    foreign key (picid) references pictures(picid) ON DELETE SET NULL
+}
+
+CREATE TABLE pictures {
+    picid int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    width int NOT NULL,
+    height int NOT NULL,
+    filepath varchar(255) NOT NULL
+}
