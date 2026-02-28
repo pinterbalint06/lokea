d8e06a6a771f15fc2bdb1f69ad9d2ffb6f9838d9
lenosza <varganorbert1605@gmail.com>
2026-02-28 15:30:42 +0100

    Fetch hibakezelés
    Hibakezelés, ha a fetch hibát dob

--- frontend/javascript/game/game-choosing.js
+++ frontend/javascript/game/game-choosing.js
@@ -31,91 +31,99 @@ async function loadGame_Maps(sort) {
     const gameMaps = await fetchURL('http://127.0.0.1:3000/api/game_maps_by_' + sort);
     let gameMapsContainer = document.getElementById('game_maps_container');
     gameMapsContainer.innerHTML = '';
-    for (let i = 0; i < gameMaps.results.length; i++) {
-        gameMapsContainer.appendChild(await createCard(gameMaps.results[i]));
-    }
-}
-
-async function fetchURL(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Hiba a játék pályák lekérésekor: ' + response.statusText);
+    if (gameMaps.success) {
+        for (let i = 0; i < gameMaps.results.length; i++) {
+            gameMapsContainer.appendChild(await createCard(gameMaps.results[i]));
         }
-        return await response.json();
-    } catch (error) {
-        console.error('Hiba a játék pályák betöltésekor:', error);
+    } else {
+        let p = document.createElement('p');
+        p.classList.add('text-center');
+        p.innerText = 'Nincsenek elérhető játékok.';
+        gameMapsContainer.appendChild(p);
     }
-}
+
+    async function fetchURL(url) {
+        let re;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Hiba a játék pályák lekérésekor: ' + response.statusText);
+            }
+            re = await response.json();
+        } catch (error) {
+            re = { success: false, results: [] };
+        }
+        return re;
+    }}
 
 
-async function createCard(game_map) {
-    let game_maps_card = document.createElement('div');
-    const image = await getCoverImage(game_map.cover_image_id);
-    game_maps_card.classList.add('card', 'glass');
-    game_maps_card.style.backgroundImage = "url('" + image + "')";
-    let game_maps_card_content = document.createElement('div');
-    game_maps_card_content.classList.add('card-content');
-    let card_name = document.createElement('h3');
-    card_name.classList.add('card-title');
-    card_name.innerText = game_map.title;
-    let card_plays = document.createElement('p');
-    card_plays.classList.add('card-desc');
-    card_plays.innerText = `Játékok száma: ${game_map.plays}`;
-    let card_created = document.createElement('p');
-    card_created.classList.add('card-desc');
-    card_created.innerText = `Létrehozva: ${(game_map.game_created.split('T')[0]).replaceAll('-', '.')}`;
-    game_maps_card_content.appendChild(card_name);
-    game_maps_card_content.appendChild(createReview(game_map.rating));
-    game_maps_card_content.appendChild(card_plays);
-    game_maps_card_content.appendChild(card_created);
-    game_maps_card.appendChild(game_maps_card_content);
-    game_maps_card.addEventListener('click', function () {
-        createModal(game_map);
-    });
-    return game_maps_card;
-}
-
-async function getCoverImage(cover_image_id) {
-    const c_image_id = {
-        image_id: cover_image_id
-    };
-    try {
-        const response = await fetch('http://127.0.0.1:3000/api/get_cover_image', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(c_image_id)
+    async function createCard(game_map) {
+        let game_maps_card = document.createElement('div');
+        const image = await getCoverImage(game_map.cover_image_id);
+        game_maps_card.classList.add('card', 'glass');
+        game_maps_card.style.backgroundImage = "url('" + image + "')";
+        let game_maps_card_content = document.createElement('div');
+        game_maps_card_content.classList.add('card-content');
+        let card_name = document.createElement('h3');
+        card_name.classList.add('card-title');
+        card_name.innerText = game_map.title;
+        let card_plays = document.createElement('p');
+        card_plays.classList.add('card-desc');
+        card_plays.innerText = `Játékok száma: ${game_map.plays}`;
+        let card_created = document.createElement('p');
+        card_created.classList.add('card-desc');
+        card_created.innerText = `Létrehozva: ${(game_map.game_created.split('T')[0]).replaceAll('-', '.')}`;
+        game_maps_card_content.appendChild(card_name);
+        game_maps_card_content.appendChild(createReview(game_map.rating));
+        game_maps_card_content.appendChild(card_plays);
+        game_maps_card_content.appendChild(card_created);
+        game_maps_card.appendChild(game_maps_card_content);
+        game_maps_card.addEventListener('click', function () {
+            createModal(game_map);
         });
-        if (response.ok) {
-            const blob = await response.blob();
-            const url = window.URL.createObjectURL(blob);
-            return url;
-        }
-    } catch (error) {
-        console.error('POST hiba:', error);
+        return game_maps_card;
     }
-}
 
-function createReview(rating) {
-    let card_rating = document.createElement('div');
-    card_rating.classList.add('stars');
-    card_rating.style.setProperty('--rating', rating);
-    return card_rating;
-}
+    async function getCoverImage(cover_image_id) {
+        const c_image_id = {
+            image_id: cover_image_id
+        };
+        try {
+            const response = await fetch('http://127.0.0.1:3000/api/get_cover_image', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(c_image_id)
+            });
+            if (response.ok) {
+                const blob = await response.blob();
+                const url = window.URL.createObjectURL(blob);
+                return url;
+            }
+        } catch (error) {
+            console.error('POST hiba:', error);
+        }
+    }
 
-function createModal(game_map) {
-    let modal = document.getElementById('myModal');
-    let modalTitle = document.getElementById('modal-title');
-    let modalStars = document.getElementById('modal-stars');
-    let modalDesc = document.getElementById('modal-desc');
-    modal.classList.add('active');
-    modalTitle.innerText = game_map.title;
-    modalStars.style.setProperty('--rating', game_map.rating);
-    modalDesc.innerText = game_map.game_description;
-    let closeBtn = document.querySelector('.modal-close-btn');
-    closeBtn.addEventListener('click', () => {
-        modal.classList.remove('active');
-    });
-}
+    function createReview(rating) {
+        let card_rating = document.createElement('div');
+        card_rating.classList.add('stars');
+        card_rating.style.setProperty('--rating', rating);
+        return card_rating;
+    }
+
+    function createModal(game_map) {
+        let modal = document.getElementById('myModal');
+        let modalTitle = document.getElementById('modal-title');
+        let modalStars = document.getElementById('modal-stars');
+        let modalDesc = document.getElementById('modal-desc');
+        modal.classList.add('active');
+        modalTitle.innerText = game_map.title;
+        modalStars.style.setProperty('--rating', game_map.rating);
+        modalDesc.innerText = game_map.game_description;
+        let closeBtn = document.querySelector('.modal-close-btn');
+        closeBtn.addEventListener('click', () => {
+            modal.classList.remove('active');
+        });
+    }
