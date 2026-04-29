34b1e635f1a6dd6f30998ddd22af95666b12688f
Pinter Balint <pinter.balint06@gmail.com>
2026-04-30 00:13:56 +0200

    server.js-ben global error handler megírása
    - apinál nem létező endpointra json-t ad vissza
    - oldalnál notfound.html-t
    - global error handler console.errorolja a hibát és /api-val kezdődő címnél jsonal más címnél majd HTML tér vissza

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const router = express.Router();
 const ip = '127.0.0.1';
 const port = 3000;
 const server = http.createServer(app);
-const onlineUsers = new Map(); 
+const onlineUsers = new Map();
 const io = new Server(server);
 
 app.use(cors());
@@ -63,7 +63,7 @@ router.get('/map', (request, response) => {
 });
 router.get('/game-maps/:gameMapId/edit',
     auth.checkAuth,
-    async (request, response) => {
+    async (request, response, next) => {
         try {
             const gameMapId = await idSchema(ERRORS.GAMEMAP.INVALID_ID).validateAsync(
                 request.params.gameMapId,
@@ -79,15 +79,9 @@ router.get('/game-maps/:gameMapId/edit',
             response.sendFile(path.join(__dirname, '../frontend/html/map-creator.html'));
         } catch (error) {
             if (error.isJoi) {
-                // TODO: valami oldal ennek
-                response.status(400).json({ error: error.details[0].message });
+                next(new AppError(error.details[0].message, 400));
             } else {
-                if (error instanceof AppError) {
-                    response.status(error.statusCode).send();
-                } else {
-                    console.error(error);
-                    response.status(500).send();
-                }
+                next(error);
             }
         }
     }
@@ -102,7 +96,7 @@ router.get('/choose_game', (request, response) => {
 router.get(
     '/game-maps/:gameMapId',
     auth.checkAuth,
-    async (request, response) => {
+    async (request, response, next) => {
         try {
             await idSchema(ERRORS.GAMEMAP.INVALID_ID).validateAsync(request.params.gameMapId, {
                 abortEarly: true,
@@ -118,39 +112,65 @@ router.get(
             response.sendFile(path.join(__dirname, '../frontend/html/game-map.html'));
         } catch (error) {
             if (error.isJoi) {
-                // TODO: valami oldal ennek
-                response.status(400).json({ error: error.details[0].message });
+                next(new AppError(error.details[0].message, 400));
             } else {
-                if (error instanceof AppError && error.statusCode == 404) {
-                    response.status(404).sendFile(path.join(__dirname, '../frontend/html/notfound.html'));
-                } else {
-                    console.error(error);
-                    response.status(500).send();
-                }
+                next(error);
             }
         }
     }
 );
-router.use((request, response) => {
-    response.status(404).sendFile(path.join(__dirname, '../frontend/html/notfound.html'));
-});
 
 //!API endpoints
 app.use(express.static(path.join(__dirname, '../frontend')));
+
 const adminEndpoints = require('./api/admin.js');
 app.use('/api/admin', adminEndpoints);
+
 const endpoints = require('./api/api.js');
 app.use('/api', endpoints);
-//!Map Creation API endpoints
+
 const mapCreationEndpoints = require('./api/mapcreator/mapcreator.js');
 app.use('/api/map-creator', mapCreationEndpoints);
-//!game maps API endpoints
+
 const gameMapsEndpoints = require('./api/gamemaps/gamemaps.routes.js');
 app.use('/api/game-maps', gameMapsEndpoints);
+
+app.use('/api', (request, response) => {
+    response.status(404).json({
+        error: ERRORS.COMMON.ENDPOINT_NOT_FOUND
+    });
+});
+
 app.use('/', router);
 
-//Socket.io
+app.use((request, response) => {
+    response.status(404).sendFile(path.join(__dirname, '../frontend/html/notfound.html'));
+});
 
+
+//! global error handler
+app.use((error, request, response, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || ERRORS.COMMON.UNEXPECTED_ERROR;
+    if (statusCode >= 500) {
+        console.error(`Unhandled Server Error: ${request.method} ${request.originalUrl}\n`, error);
+    }
+
+    if (!response.headersSent) {
+        if (request.originalUrl.startsWith('/api')) {
+            response.status(statusCode).json({
+                error: message
+            });
+        } else {
+            response.status(statusCode).send(`ide valahogy html`); // TODO
+        }
+    } else {
+        next(error);
+    }
+});
+
+
+//Socket.io
 io.on("connection", (socket) => {
     const session = socket.request.session;
     const userId = session ? session.userid : null;
@@ -161,7 +181,7 @@ io.on("connection", (socket) => {
         if (!onlineUsers.has(userId)) {
             onlineUsers.set(userId, new Set());
             onlineUsers.get(userId).add(socket.id);
-            
+
             io.emit("totalOnline", onlineUsers.size);
         } else {
             onlineUsers.get(userId).add(socket.id);
