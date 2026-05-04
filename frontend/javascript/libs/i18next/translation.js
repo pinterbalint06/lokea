4b72dc0b73fdef2a9f3f0a4b4fd89e2db6124bd4
ErikEordogh <eordogh.erik@gmail.com>
2026-05-04 02:24:23 +0200

    Html-ek javitása 2

--- frontend/javascript/libs/i18next/translation.js
+++ frontend/javascript/libs/i18next/translation.js
@@ -9,6 +9,10 @@ export function translatePage() {
         const key = element.getAttribute('data-i18n-label');
         element.dataset.label = i18next.t(key) + ':';
     });
+    document.querySelectorAll('[data-i18n-placeholder]').forEach(element => {
+        const key = element.getAttribute('data-i18n-placeholder');
+        element.setAttribute('placeholder', i18next.t(key));
+    });
     document.documentElement.lang = i18next.language;
     const titleKey = document.querySelector('title')?.getAttribute('data-i18n');
     if (titleKey) {
@@ -18,12 +22,10 @@ export function translatePage() {
 
 export async function nyelvSzinkronizalas() {
     try {
-        //todo - pull request utan vissza
-        // let response = await fetch('/api/users/language');
-        // let data = await response.json();
-        // if (!response.ok) throw new Error(data);
-        await initI18next('en');
-        return 'en';
+        let response = await fetch('/api/users/language');
+        let data = await response.json();
+        if (!response.ok) throw new Error(data);
+        await initI18next(data.language);
     } catch (error) {
         console.log(error.message);
         throw error;
